786abde355c82ebece3fe3432dee23696f218496
Sayfiddin <sayfiddin.gofurov@inbox.ru>
2023-01-05 17:52:16 +0500

    uninstall react-yandex

--- src/components/Generic/Yandex/index.jsx
+++ src/components/Generic/Yandex/index.jsx
@@ -11,10 +11,10 @@ const mapStyle = {
 };
 
 const Yandex = ({center}) => {
-  
+  const mapstate={center:center|| [ 41.311081, 69.240562], zoom: 12}
   return (
     <YMaps query={{ apikey: "afbb60c1-0761-48a5-b821-b566bf220d8b", lang: "EN" }} >
-      <Map style={mapStyle} defaultState={{center:center|| [ 41.311081, 69.240562], zoom: 12} }>
+      <Map style={mapStyle} state={mapstate}>
        <Placemark geometry={[41.311081, 69.240562]}/>
       </Map>
     </YMaps>
