b9ec5411c112bf9d8f42cb5505a0bd0ee340610d
Sayfiddin <sayfiddin.gofurov@inbox.ru>
2023-01-05 17:47:27 +0500

    change yandex-map

--- src/components/Generic/Yandex/index.jsx
+++ src/components/Generic/Yandex/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { YMaps, Map,Placemark} from "react-yandex-maps";
+import { YMaps, Map,Placemark} from "@pbe/react-yandex-maps";
 
 const mapStyle = {
   position: "relative",
@@ -9,13 +9,13 @@ const mapStyle = {
   height: "50vh",
   overflow: "hidden",
 };
-// query={{ apikey: "afbb60c1-0761-48a5-b821-b566bf220d8b", lang: "EN" }}
+
 const Yandex = ({center}) => {
   
   return (
-    <YMaps >
+    <YMaps query={{ apikey: "afbb60c1-0761-48a5-b821-b566bf220d8b", lang: "EN" }} >
       <Map style={mapStyle} defaultState={{center:center|| [ 41.311081, 69.240562], zoom: 12} }>
-     <Placemark geometry={[41.311081, 69.240562]}/>
+       <Placemark geometry={[41.311081, 69.240562]}/>
       </Map>
     </YMaps>
   );
